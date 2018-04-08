76296c14a7994ba0b472f3b61c7c4c26ec10cbf2
reficul31 <reficul31@gmail.com>
2018-04-08 12:08:38 +0530

    Redifining the models

--- app/models.go
+++ app/models.go
@@ -1,10 +1,27 @@
 package app
 
-import "time"
+import (
+	"time"
+)
+
+// User describes the user structure
+type User struct {
+	ID          int     `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
+	Name        string  `json:"name" sql:"not null"`
+	Email       string  `json:"email" sql:"not null;unique"`
+	Password    string  `json:"password,omitempty" sql:"type:binary(20);not null"`
+	Gender      string  `json:"gender, omitempty"`
+	Profile     string  `json:"profile"`
+	AccessLevel int     `json:"access_level,omitempty" sql:"not null;DEFAULT:0"`
+	Albums      []Album `json:"albums,omitempty"`
+}
+
+type Users []User
 
 // Photo describes the photo structure
 type Photo struct {
 	ID          int       `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
+	AlbumID     uint      `json:"album_id" sql:"not null"`
 	Description string    `json:"description" sql:"not null"`
 	Created     time.Time `json:"created, omitempty" sql:"not null;DEFAULT:current_timestamp"`
 	Privacy     int       `json:"privacy" sql:"not null; DEFAULT:0"`
@@ -14,6 +31,7 @@ type Photo struct {
 // Album describes the album structure
 type Album struct {
 	ID          int       `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
+	UserID      uint      `json:"user_id" sql:"not null"`
 	Description string    `json:"description" sql:"not null"`
 	Created     time.Time `json:"created, omitempty" sql:"not null;DEFAULT:current_timestamp"`
 	Photos      []Photo   `json:"photos"`
@@ -21,12 +39,17 @@ type Album struct {
 	Likes       int       `json:"likes" sql:"DEFAULT:0"`
 }
 
-// User describes the user structure
-type User struct {
-	ID       int    `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
-	Name     string `json:"name" sql:"not null"`
-	Email    string `json:"email" sql:"not null;unique"`
-	Password string `json:"password,omitempty" sql:"type:binary(20);not null"`
-	Gender   string `json:"gender, omitempty"`
-	Profile  string `json:"profile"`
+
+type ForgotPassword struct {
+	ID        string    `json:"id" gorm:"primary_key"`
+	UserID    int       `json:"user_id" sql:"not null"`
+	Timestamp time.Time `json:"timestamp" sql:"not null;DEFAULT:current_timestamp"`
+}
+
+type CurrentUser struct {
+	ID          int    `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Email       string `json:"email,omitempty"`
+	AccessLevel int    `json:"access_level,omitempty"`
+	LoggedIn    bool   `json:"logged_in,omitempty"`
 }
--- migrate.go
+++ migrate.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"log"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/reficul31/codechef-photo-gallery/app"
+	"github.com/jinzhu/gorm"
 )
 
 // @todo Implement a better migration scheme
@@ -19,14 +19,19 @@ func main() {
 			ServerAddr: ":8000",
 			DBUsername: "root",
 			DBPassword: "reficul31",
-			DBName:     "hawk",
+			DBName:     "gallery",
 		}
 	}
 
-	db, err := gorm.Open("sqlite3", "gallery.db")
+	connectionString := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		config.DBUsername,
+		config.DBPassword,
+		config.DBName)
+	db, err := gorm.Open("mysql", connectionString)
 	defer db.Close()
+	db.AutoMigrate(&app.User{}, &app.ForgotPassword{}, &app.Photo{}, &app.Album{})
+	db.Model(&app.Album{}).Related(&app.Photo{})
+	db.Model(&app.User{}).Related(&app.Album{})
 
-	db.AutoMigrate(&app.Tag{}, &app.Post{})
-	db.Model(&app.Album{}).Related(&app.Photos{}, "Photos")
 	log.Println("Migration Successful")
 }
