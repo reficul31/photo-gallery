dd489515565100a99e198175d7cabaa05e30f1ce
reficul31 <reficul31@gmail.com>
2018-04-11 23:22:17 +0530

    Front end tweaks

--- gallery/routes.js
+++ gallery/routes.js
@@ -2,6 +2,13 @@ export default [
   {
     name: 'Album',
     pathname: '/gallery/album',
+    subPaths: ['/gallery/album/view', '/gallery/album/edit', '/gallery/album/add'],
+    navigation: true,
+  },
+  {
+    name: 'Profile',
+    pathname: '/gallery/user',
+    subPaths: ['/gallery/user/view', '/gallery/user/edit'],
     navigation: true,
   },
 ]
